7220b6bca1262b122214efdd4671db9c17eba761
stansharon7 <168943435+stansharon7@users.noreply.github.com>
2025-03-26 09:15:59 -0400

    fixed the fav page

--- frontend/src/pages/Favourites.jsx
+++ frontend/src/pages/Favourites.jsx
@@ -2,25 +2,29 @@ import "../css/Favourites.css"
 import { useMovieContext } from "../context/MovieContext"
 import MovieCard from "../components/MovieCard"
 
-function Favourites(){
-    const {favourites} = useMovieContext();
+function Favourites() {
+    const { favourites } = useMovieContext();
 
-    if (favourites) {
+    if (favourites.length > 0) { // Ensure it has items
         return (
-        <div className= "Fvaourites"> 
-            <h2>Your Favourites</h2>
-            <div className= "movies-grid">
-            {favourites.map((movie) => (
-                <MovieCard movie= {movie} key={movie.id} />
-            ))}
-        </div>
-    </div>
-    )}
+            <div className="Favourites"> 
+                <h2>Your Favourites</h2>
+                <div className="movies-grid">
+                    {favourites.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 
-    return <div className= "favourites-empty">
-        <h2>No Favourites Movies Yet</h2>
-        <p>Start adding movies to your favourites and they will appear here!</p>
-    </div>
+    return (
+        <div className="favourites-empty">
+            <h2>No Favourite Movies Yet</h2>
+            <p>Start adding movies to your favourites and they will appear here!</p>
+        </div>
+    );
 }
+
 
 export default Favourites
